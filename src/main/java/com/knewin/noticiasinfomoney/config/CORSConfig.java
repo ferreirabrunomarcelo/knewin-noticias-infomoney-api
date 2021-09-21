8d9637128848121667875dcc948583432230f482
Bruno <ferreira.brunomarcelo@gmail.com>
2021-09-21 03:25:39 -0300

    feat: add CORS settings

---
+++ src/main/java/com/knewin/noticiasinfomoney/config/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.knewin.noticiasinfomoney.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    
+    @Bean
+    public WebMvcConfigurer getCorsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+            }
+        };
+    }
+}
