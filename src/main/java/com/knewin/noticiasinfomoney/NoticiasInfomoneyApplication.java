4bf6906d872b3c812a5f8524f9fc9774588fc0c9
Bruno <ferreira.brunomarcelo@gmail.com>
2021-09-21 03:31:42 -0300

    feat: add maven dependencies

---
+++ src/main/java/com/knewin/noticiasinfomoney/NoticiasInfomoneyApplication.java
@@ -0,0 +1,12 @@
+package com.knewin.noticiasinfomoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasInfomoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasInfomoneyApplication.class, args);
+	}
+}
